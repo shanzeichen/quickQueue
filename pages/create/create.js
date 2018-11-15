572b438f57cc5c3862abdeeabcc2e2ba80c3e64e
shanzeichen <shanzeichen@163.com>
2018-11-15 23:27:10 +0800

    add activity code and its qrcode

--- pages/create/create.js
+++ pages/create/create.js
@@ -5,6 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
+    qr_src:"../../images/home.png",
+    qr_hidden: true,
     item: {
       name:'initial name',
       kind: true,
@@ -12,58 +14,58 @@ Page({
       detail: ''
     }
   },
-  updateName: function(e){
-    let itemTemp = this.data.item;
-    itemTemp['name'] = e.detail.value;
-    this.setData({
-      item: itemTemp
+  confirmItem: function(e){
+    var that = this;
+    that.setData({
+      item: e.detail.value
     })
-  },
-  updateKind: function(e){
-    let itemTemp = this.data.item;
-    itemTemp['kind'] = e.detail.value;
-    this.setData({
-      item: itemTemp
-    })
-  },
-  updateLimit: function(e){
-    let itemTemp = this.data.item;
-    itemTemp['limit'] = e.detail.value;
-    this.setData({
-      item: itemTemp
-    })
-  },
-  updateDetail: function(e){
-    let itemTemp = this.data.item;
-    itemTemp['detail'] = e.detail.value;
-    this.setData({
-      item: itemTemp
-    })
-  },
-  confirmItem: function(){
-    let confirmItem = '\n活动名称：' + this.data.item.name
-                    + '\n活动类型：' + (this.data.item.kind?'临时':'长期')
-                    + '\n限定排号：' + this.data.item.limit
-                    + '\n活动介绍：' + this.data.item.detail;
-    let itemTemp = this.data.item;
-    wx.showModal({
-      title: '活动详情',
-      content: confirmItem,
-      success (res){
-        if (res.confirm){
-          let item_no = Math.floor(100000+Math.random()*900000);
-          wx.setStorageSync(item_no + '', itemTemp);
-          wx.showToast({
-            title: '创建成功',
-            icon: 'success'
-          })
-        }else if(res.cancel){
-          wx.showToast({
-            title: '取消创建活动',
-          })
-        }
+    wx.request({
+      url: 'http://192.168.191.2:8000/quickQueue/create',
+      data: this.data.item,
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header 默认是application/json
+      success: function (res) {
+        wx.navigateTo({
+          url: 'create_success?item_id='+res.data['ItemID'],
+        })
+        // 操作json数据
+        // that.setData({
+        //   qr_src: 'http://192.168.191.2:8000/static/images/qr/'+res.data['ItemID']+'.png',
+        //   qr_hidden: false
+        // })
+        // console.log(res);
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
       }
     })
+    console.log(e.detail)
+    // let confirmItem = '\n活动名称：' + this.data.item.name
+    //                 + '\n活动类型：' + (this.data.item.kind?'临时':'长期')
+    //                 + '\n限定排号：' + this.data.item.limit
+    //                 + '\n活动介绍：' + this.data.item.detail;
+    // let itemTemp = this.data.item;
+    // wx.showModal({
+    //   title: '活动详情',
+    //   content: confirmItem,
+    //   success (res){
+    //     if (res.confirm){
+    //       let item_no = Math.floor(100000+Math.random()*900000);
+    //       wx.setStorageSync(item_no + '', itemTemp);
+    //       wx.showToast({
+    //         title: '创建成功',
+    //         icon: 'success'
+    //       })
+    //     }else if(res.cancel){
+    //       wx.showToast({
+    //         title: '取消创建活动',
+    //       })
+    //     }
+    //   }
+    // })
   },
 
   /**
